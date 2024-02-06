bd8e8d376ccc07839b16aae4ec21ba2962d29171
abreham0923 <abreshcul@gmail.com>
2024-02-06 05:02:29 -0500

    First Commit

---
+++ codes/script.js
@@ -0,0 +1,219 @@
+/*=============== search MENU ===============*/
+const searchButton = document.getElementById("search-button"),
+  searchClose = document.getElementById("search-close"),
+  searchContent = document.getElementById("search-content");
+
+/* Menu show */
+if (searchButton) {
+  searchButton.addEventListener("click", () => {
+    searchContent.classList.add("show-search");
+  });
+}
+
+/* Menu hidden */
+if (searchClose) {
+  searchClose.addEventListener("click", () => {
+    searchContent.classList.remove("show-search");
+  });
+}
+
+/*=============== login MENU ===============*/
+const loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close"),
+  loginContent = document.getElementById("login-content");
+
+/* Menu show */
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    loginContent.classList.add("show-login");
+  });
+}
+
+/* Menu hidden */
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    loginContent.classList.remove("show-login");
+  });
+}
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
+
+// swiper
+let swiperHome = new Swiper(".home__swiper", {
+  loop: true,
+  spaceBetween: -24,
+  grabCusor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  autoplay: {
+    delay: 1500,
+    disableOnIneraction: false,
+  },
+  breakpoints: {
+    1220: {
+      spaceBetween: -32,
+    },
+  },
+});
+// featured
+
+let swiperFeatured = new Swiper(".featured__swiper", {
+  loop: true,
+  spaceBetween: -16,
+  grabCusor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 4,
+      centeredSlides: false,
+    },
+  },
+});
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+//
+
+let SwiperTestimonial = new Swiper(".testimonial__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCusor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  autoplay: {
+    delay: 1500,
+    disableOnIneraction: false,
+  },
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 4,
+      centeredSlides: false,
+    },
+  },
+});
+
+//
+
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+////
+
+let preveiwContainer = document.querySelector(".prodocts-preview");
+let previewBox = preveiwContainer.querySelectorAll(".preview");
+
+document
+  .querySelectorAll(".products-containers .prodoct")
+  .forEach((prodoct) => {
+    prodoct.onclick = () => {
+      preveiwContainer.style.display = "flex";
+      let name = prodoct.getAttribute("data-name");
+      previewBox.forEach((preview) => {
+        let target = preview.getAttribute("data-target");
+        if (name == target) {
+          preview.classList.add("active"); // Fix the typo here
+        }
+      });
+    };
+  });
+
+preveiwContainer.scrollIntoView({ behavior: "smooth", block: "center" });
+
+previewBox.forEach((close) => {
+  close.querySelector("#view-close").onclick = () => {
+    close.classList.remove("active");
+    preveiwContainer.style.display = "none";
+  };
+});
+// 
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal('.home__data, .featured__container, .join__data, .discount__data, .testimonial__container')
+sr.reveal('.home__images', {delay: 600})
+sr.reveal('.services__card', {interval: 100})
+sr.reveal('.discount__data', {origin: left})
+sr.reveal('.discount__images', {origin: right})
